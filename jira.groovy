4170a21d6feebadfe2a9ceed1677c094dc94ec43
commscope-qa <79118815+commscope-qa@users.noreply.github.com>
2023-11-01 16:45:22 -0700

    Intial check in

---
+++ jira.groovy
@@ -0,0 +1,238 @@
+def strGetRequest() {
+	return '''
+		def sendGetRequest(url, username, password) {
+			def conn = new URL(url).openConnection()
+			conn.setRequestProperty("Content-Type", "application/json")
+			conn.setRequestProperty("Accept", "application/json")
+			conn.setRequestProperty("Authorization", "Basic " + (username+":"+password).bytes.encodeBase64().toString())
+			conn.requestMethod = 'GET'
+			def code = conn.getResponseCode()
+			def content = conn.getInputStream().getText()
+			return [code, content]
+		}
+	'''
+}
+
+def strJiraProjectVersion() {
+	return '''
+		import jenkins.model.Jenkins
+		import hudson.model.User
+		import groovy.json.JsonOutput
+		import groovy.json.JsonSlurper
+		
+		def user = User.current().getId()
+		def filename = "/tmp/project_version_" + user + ".json"
+
+		if (RetrieveProjectVersion == "Retrieve Project Version") {
+			// Get Etrack username and password from global credentials
+			def username = ""
+			def password = ""
+			def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+				com.cloudbees.plugins.credentials.common.StandardUsernameCredentials.class,
+				Jenkins.instance,
+			null,
+			null
+		)
+		for (c in creds) {
+			if (c.id == "etrack_creds_" + user) {
+				username = c.username
+				password = c.password
+			}
+		}   
+		
+		def jiraBaseURL = "https://odart.arrisi.com"
+		def project = "ECOIOT"
+	
+		def projectId = ""
+		def versionId = ""
+		def versionNameId= [:]
+		def versionList = []
+		
+		def jsonSlurper = new JsonSlurper()
+		def url = jiraBaseURL+"/rest/api/latest/project/"+project+"/versions"
+		content = sendGetRequest(url, username, password)
+		jsonResponse = jsonSlurper.parseText(content)
+		jsonResponse.each {
+			if (it.released == false) {
+				versionList.add(it.name)
+				projectId = it.projectId
+				versionId = it.id
+				versionNameId.put(it.name, versionId)
+			}
+		}
+	
+		def data = [
+			projectId: projectId,
+			versionNameId: versionNameId
+		]
+		def json_str = JsonOutput.toJson(data)
+		def json_beauty = JsonOutput.prettyPrint(json_str)                            	
+			
+		File file = new File(filename)
+		file.write(json_beauty)
+		
+		return versionList
+	}                    
+	'''
+}
+
+
+def strJiraProjectCycle() {
+	return '''
+		import jenkins.model.Jenkins
+		import hudson.model.User
+		import groovy.json.JsonOutput
+		import groovy.json.JsonSlurper
+		
+		def user = User.current().getId()
+		def filename = "/tmp/project_version_" + user + ".json"
+
+		def jsonSlurper = new JsonSlurper()
+		def data = jsonSlurper.parse(new File(filename))
+		
+		def jiraBaseURL = "https://odart.arrisi.com"
+		def projectId =  data["projectId"]
+		def versionId = data["versionNameId"][ProjectVersion]
+		def cycleNameId = [:]
+		def cycleNameList = []
+		
+		// Get Etrack username and password from global credentials
+		def username = ""
+		def password = ""
+		def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+			com.cloudbees.plugins.credentials.common.StandardUsernameCredentials.class,
+			Jenkins.instance,
+			null,
+			null
+		)
+		for (c in creds) {
+			if (c.id == "etrack_creds_" + user) {
+				username = c.username
+				password = c.password
+			}
+		}
+		
+		def url = jiraBaseURL+"/rest/zapi/latest/cycle?projectId="+projectId+"&versionId="+versionId
+		content = sendGetRequest(url, username, password)
+		//assert code == 200
+		
+		jsonResponse = jsonSlurper.parseText(content)
+		jsonResponse.each {
+			if (it.key != "recordsCount") {
+				cycleNameList.add(it.value.name)
+				cycleNameId.put(it.value.name, it.key)
+			}
+		}
+		
+		def data1 = [
+				projectId: projectId,
+				versionId: versionId,
+				cycleNameId: cycleNameId
+			]
+		def json_str = JsonOutput.toJson(data1)
+		def json_beauty = JsonOutput.prettyPrint(json_str)
+		filename = "/tmp/project_cycle_" + user + ".json"
+		File file = new File(filename)
+		file.write(json_beauty)
+		
+		return cycleNameList                   
+	''' 
+}
+
+def strJiraProjectFolder() {
+	return '''
+		import jenkins.model.Jenkins
+		import hudson.model.User
+		import groovy.json.JsonOutput
+		import groovy.json.JsonSlurper
+		
+		def user = User.current().getId()
+		def filename = "/tmp/project_cycle_" + user + ".json"
+
+		def jsonSlurper = new JsonSlurper()
+		def data = jsonSlurper.parse(new File(filename))
+		
+		def jiraBaseURL = "https://odart.arrisi.com"
+		def projectId =  data["projectId"]
+		def versionId = data["versionId"]
+		def cycleId = data["cycleNameId"][ProjectCycle]
+		def folderNameList = [""]
+		
+		// Get Etrack username and password from global credentials
+		def username = ""
+		def password = ""
+		def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+			com.cloudbees.plugins.credentials.common.StandardUsernameCredentials.class,
+			Jenkins.instance,
+			null,
+			null
+		)
+		for (c in creds) {
+			if (c.id == "etrack_creds_" + user) {
+				username = c.username
+				password = c.password
+			}
+		}
+		
+		def url = jiraBaseURL+"/rest/zapi/latest/cycle/"+cycleId+"/folders?projectId="+projectId+"&versionId="+versionId
+		content = sendGetRequest(url, username, password)
+		//assert code == 200
+		
+		jsonResponse = jsonSlurper.parseText(content)
+		jsonResponse.each {
+			folderNameList.add(it.folderName)
+		}
+		
+		return folderNameList                   
+	'''    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def sendGetRequest(url, username, password) {
+	println("url = " + url)
+	def conn = new URL(url).openConnection()
+	conn.setRequestProperty("Content-Type", "application/json")
+	conn.setRequestProperty("Accept", "application/json")
+	conn.setRequestProperty("Authorization", "Basic " + (username+":"+password).bytes.encodeBase64().toString())
+	conn.requestMethod = 'GET'
+	def code = conn.getResponseCode()
+	def content = conn.getInputStream().getText()
+	//println("http code" + code)
+	return [code, content]
+}
+
+def sendPutRequest(url, body, username, password) {
+	println("url = " + url)
+	println("body = " + body)
+	def conn = new URL(url).openConnection()
+	conn.setRequestProperty("Content-Type", "application/json")
+	conn.setRequestProperty("Accept", "application/json")
+	conn.setRequestProperty("Authorization", "Basic " + (username+":"+password).bytes.encodeBase64().toString())
+	conn.requestMethod = 'PUT'
+
+	conn.setDoOutput(true)
+	OutputStream outStream = conn.getOutputStream()
+	OutputStreamWriter outStreamWriter = new OutputStreamWriter(outStream, "UTF-8")
+	outStreamWriter.write(body)
+	outStreamWriter.flush()
+	outStreamWriter.close()
+	outStream.close()
+
+	def code = conn.getResponseCode()
+	def content = conn.getInputStream().getText()
+	//println("http code" + code)	
+	return [code, content]
+}  
+
